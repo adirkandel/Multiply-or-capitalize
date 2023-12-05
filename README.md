95a8273439d159dfd2fda85985de7c77fa897750
adirkandel <adirkandel@gmail.com>
2023-12-05 21:56:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Multiply-or-capitalize
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/adirkandel/Multiply-or-capitalize)
---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @description
+ * Gets an array of numbers or objects with name,
+ * will multiply the numbers or capitalize the object name
+ * @param {(number|{name: string})[]} arr
+ * @return {(number|{name: string})[]}
+ */
+function multiplyOrCapitalize(arr) {}
